9f2a8c790d5f18dcca78add0cc4f401315799a8f
Prabuddha <prabuddha.proart@gmail.com>
2021-01-30 13:15:46 +0530

    domain, repository and DTO

---
+++ src/main/java/prabu/rest/restapi/api/vi/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package prabu.rest.restapi.api.vi.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/prabu/rest/restapi/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package prabu.rest.restapi.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import prabu.rest.restapi.domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
